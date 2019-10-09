f118311d678cac0feb51f42c0360d76ae9b6145f
Felix Gerberding <gerberding.felix@gmail.com>
2019-10-10 01:42:57 +0200

    Basic language file structure

---
+++ de/verification.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'meta' => [
+        'title' => 'Verifizierung - OpossumTS.net',
+        //'description' => '',
+    ],
+    'home' => [
+        'confirm_idendity' => 'Bestätige deine Idendität',
+        'teamspeak_name' => 'Wie ist dein Name im Teamspeak?',
+        'confirm' => 'bestätigen',
+        'continue' => 'weiter',
+        'step' => [
+          'rules' => [
+
+          ],
+          'verify' => [
+
+          ],
+          'client_selection' => [
+            
+          ]
+        ]
+    ],
+    'error' => [
+        'not_online' => 'Das System konnte dich nicht auf dem Server finden.<br/>Bitte verbinde dich zum Server und gib deine UUID ein, damit dir die Gruppen zugewiesen werden können.'
+    ]
+];
