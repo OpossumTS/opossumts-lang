3edb87dffdc923448baaf69cce925851a824230f
Felix Gerberding <gerberding.felix@gmail.com>
2019-10-15 19:09:26 +0200

    french basics
    added basic files + pages for french

---
+++ fr/verification.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'meta' => [
+        'title' => 'Verification - OpossumTS.net',
+        'description' => '',
+    ],
+    'home' => [
+        'confirm_idendity' => 'Confirm your Idendity',
+        'teamspeak_name' => 'What\'s your name on TeamSpeak?',
+        'confirm' => 'confirm',
+        'continue' => 'continue',
+        'step' => [
+          'rules' => [
+
+          ],
+          'verify' => [
+
+          ],
+          'client_selection' => [
+
+          ]
+        ]
+    ],
+    'error' => [
+        'not_online' => 'We coud\'nt find you on our TeamSpeak.<br/>Please reconnect or insert your UUID manually.'
+    ]
+];
